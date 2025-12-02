9309fe8966d90bf7222c65196becf46b23808629
Sadman kabir <sadmankabir351@gmail.com>
2025-12-02 16:54:15 +0600

    some data fetched

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(cors())
 app.use(express.json())

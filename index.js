27d78a0e09b9c986ec240374c7a48b1f1d8fc470
Sadman kabir <sadmankabir351@gmail.com>
2025-12-06 21:04:59 +0600

    partners data added

--- index.js
+++ index.js
@@ -23,6 +23,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db('studymate-db')
+    const partnersCollection = db.collection('partners')
+
+    app.get('/partners', async (req, res) => {
+
+      const result = await partnersCollection.find().toArray()
+
+      res.send(result)
+
+    })
+    
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
